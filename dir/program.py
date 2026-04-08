4e5e804025da8a7405313a869a53b517eff4fc51
RajpootDeepak <insta8219743056>
2026-04-09 00:06:13 +0530

    working on cost optimization project

---
+++ dir/program.py
@@ -0,0 +1,51 @@
+# this is just a practice work of mine, which i done during this small project journey  ...... 
+# why should I manually create multiple ec2 instances, volumes or snapshot ...
+# lets implement what i'm learning  ... 
+import boto3
+ec2 = boto3.client('ec2')
+
+# --- Timeline 1: Create EC2 Instances ---
+# This block creates multiple EC2 instances.
+response = ec2.run_instances(
+    ImageId='any-os-image-you-want',
+    MinCount=1,
+    MaxCount=7,
+    InstanceType='t2.nano', 
+    NetworkInterfaces=[
+        {
+            'SubnetId': 'your-own-sub-id',  
+            'DeviceIndex': 0,
+            'AssociatePublicIpAddress': True, 
+            'Groups': ['enter-ur-own-security-group-id'] 
+        }
+    ],
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'Testing-series'
+                }
+            ]
+        }
+    ]
+)
+ 
+# --- Timeline 2: Create Snapshots for All Volumes ---
+# This block is independent and creates snapshots for ALL EBS volumes in the account.
+# It is not limited to volumes from the instances created above.
+# This represents a different point in time or a separate automation task.
+
+list = ec2.describe_volumes()
+for volume in list['Volumes'] :
+    ec2.create_snapshot(
+         VolumeId= volume['VolumeId'],
+        Description='Snapshot of {}'.format(volume['VolumeId'])
+    )
+
+# for deleting ec2 instance or instances ....
+# instanceid = input("enter intance id :").strip()
+# client.terminate_instances(
+#     InstanceIds=[instanceid]
+# )   
